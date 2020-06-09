e1cbabf5f70f332870602a2f3663c07d3e2d6e4e
xujiyou <xujiyou555@gmail.com>
2020-06-09 14:07:46 +0800

    zk加入监控

--- drift-zookeeper/build.sh
+++ drift-zookeeper/build.sh
@@ -3,5 +3,5 @@
 #docker build -t hacksoul/drift-zookeeper:v0.0.13  ./
 #docker push hacksoul/drift-zookeeper:v0.0.13
 
-docker build -t registry.prod.bbdops.com/common/drift-zookeeper:v0.0.13  ./
-docker push registry.prod.bbdops.com/common/drift-zookeeper:v0.0.13
+docker build -t registry.prod.bbdops.com/common/drift-zookeeper:v0.0.14  ./
+docker push registry.prod.bbdops.com/common/drift-zookeeper:v0.0.14
--- drift-zookeeper/script/start-zookeeper.sh
+++ drift-zookeeper/script/start-zookeeper.sh
@@ -7,6 +7,11 @@ echo "syncLimit=5" >> /opt/zookeeper/conf/zoo.cfg
 echo "clientPort=2181" >> /opt/zookeeper/conf/zoo.cfg
 echo "dataDir=$DATA_DIR" >> /opt/zookeeper/conf/zoo.cfg
 
+# 配置监控
+echo "metricsProvider.className=org.apache.zookeeper.metrics.prometheus.PrometheusMetricsProvider" >> /opt/zookeeper/conf/zoo.cfg
+echo "metricsProvider.httpPort=7000" >> /opt/zookeeper/conf/zoo.cfg
+echo "metricsProvider.exportJvmInfo=true" >> /opt/zookeeper/conf/zoo.cfg
+
 # 集群配置
 DOMAIN=$(hostname -d)
 for (( i=1; i<=$SERVER_NUMBER; i++ )); do
