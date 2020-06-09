fdcceb6f856f7338f70d05a8a4ffa08448b5b938
xujiyou <xujiyou555@gmail.com>
2020-06-09 10:11:09 +0800

    readme

---
+++ README.md
@@ -0,0 +1,17 @@
+# Drift-images
+
+## 支持的组件
+
+- [x] ZooKeeper 3.6.1
+- [ ] Kafka
+- [ ] Yarn
+- [ ] HDFS
+- [ ] HBase
+- [ ] Hive
+- [ ] Spark
+- [ ] Flink
+
+## 最新镜像版本
+
+- [x] registry.prod.bbdops.com/common/drift-zookeeper:v0.0.13
+
--- drift-zookeeper/build.sh
+++ drift-zookeeper/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-#docker build -t hacksoul/drift-zookeeper:v0.0.12  ./
-#docker push hacksoul/drift-zookeeper:v0.0.12
+#docker build -t hacksoul/drift-zookeeper:v0.0.13  ./
+#docker push hacksoul/drift-zookeeper:v0.0.13
 
 docker build -t registry.prod.bbdops.com/common/drift-zookeeper:v0.0.13  ./
 docker push registry.prod.bbdops.com/common/drift-zookeeper:v0.0.13
