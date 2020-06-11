df4131fcfa50a18ec50be8d382496a4d0047cfe5
xujiyou <xujiyou555@gmail.com>
2020-06-11 14:26:59 +0800

    更改spark启动脚本

--- drift-spark/master/script/start-spark-master.sh
+++ drift-spark/master/script/start-spark-master.sh
@@ -3,5 +3,6 @@
 mv /opt/spark/conf/spark-env.sh.template /opt/spark/conf/spark-env.sh
 echo "export SPARK_DAEMON_JAVA_OPTS='-Dspark.deploy.recoveryMode=ZOOKEEPER  -Dspark.deploy.zookeeper.url=$ZOOKEEPER_ADDRESS  -Dspark.deploy.zookeeper.dir=/spark'" >> /opt/spark/conf/spark-env.sh
 echo "SPARK_NO_DAEMONIZE=true" >> /opt/spark/conf/spark-env.sh
+echo "SPARK_MASTER_PORT=7077" >> /opt/spark/conf/spark-env.sh
 
 start-master.sh
