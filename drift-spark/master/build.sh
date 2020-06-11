9474fd4e66b0c1f9011a4d374e95ae20eb4577f1
xujiyou <xujiyou555@gmail.com>
2020-06-11 12:51:53 +0800

    镜像修改

--- drift-spark/master/build.sh
+++ drift-spark/master/build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-docker build -t registry.prod.bbdops.com/common/drift-spark-master:v0.0.1  ./
-docker push registry.prod.bbdops.com/common/drift-spark-master:v0.0.1
+docker build -t registry.prod.bbdops.com/common/drift-spark-master:v0.0.2  ./
+docker push registry.prod.bbdops.com/common/drift-spark-master:v0.0.2
